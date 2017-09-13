9ddfb6fc0d26ba0df063292d2f46e6fe3a718d56
Henry Hsu <h_hsu817@yahoo.com>
2017-09-13 19:51:02 +0000

    Use 'puts' to output string to stdout

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main -       Use 'puts' to output a string to stdout
+ *
+ * Description: prints ' "Programming is like building a multilingual puzzle '
+ *              to stdout
+ *
+ * Return:      Returns '0' if output string to stdout with no issue
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -3,3 +3,6 @@
 1-compiler: Run c source file through GCC, stop after compilation stage
 
 2-assembler: Run c sourcefile through GCC, stop after assembling stage
+
+4-puts.c: Use 'puts' to print string to stdout
+
