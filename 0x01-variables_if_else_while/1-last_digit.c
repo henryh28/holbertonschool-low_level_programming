d79bc451b922d5ab2b5dfda304e554e80b956729
Henry Hsu <h_hsu817@yahoo.com>
2017-09-14 09:46:28 +0000

    Prints last digit of variable & associated string

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Output string based on conditional expressions
+ *
+ * Description: Outputs different strings based on variable 'n' being
+ * greater than 5, zero, or less than 6 and not zero.
+ *
+ * Return: (0) Successful execution
+ */
+
+int main(void)
+{
+	int n;
+	int last_digit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	last_digit = n % 10;
+
+	if (last_digit > 5)
+	{
+		printf("Last digit of %d is %d and is greater tan 5\n", n, last_digit);
+	}
+	else if (last_digit == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, last_digit);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
+	}
+
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,4 +1,6 @@
 0-positive_or_negative.c - conditional express exercise
 
+1-last_digit.c - Print out last digit of variable and conditional string associated with its value
+
 
 
