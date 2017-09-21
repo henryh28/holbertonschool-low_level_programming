10ee55e6417b1faabfdc5d47fbad16ec32caf202
Henry Hsu <h_hsu817@yahoo.com>
2017-09-21 15:44:24 +0000

    Returns result of multiplying 2 integer arguments

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * mul - Multiplies 2 integer arguments
+ *
+ * @a: First integer argument. To be multiplied with @b
+ * @b: Second integer argument. To be multiplied with @a
+ *
+ * Return: (0) Success
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
