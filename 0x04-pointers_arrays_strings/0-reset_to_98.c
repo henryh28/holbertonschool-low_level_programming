814b5b4702c8f5185967c4b72bd74ac148748238
Henry Hsu <h_hsu817@yahoo.com>
2017-09-25 19:19:47 +0000

    Update function parameter value through the use of pointer

---
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - Use pointer to update value of int to 98
+ *
+ * @n:           int variable whose value need to be updated to 98
+ *
+ * Return:       None
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
