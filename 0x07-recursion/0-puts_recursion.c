8748088ccd7837686d5162c13af3287ed54fb93f
Henry Hsu <h_hsu817@yahoo.com>
2017-10-04 08:20:52 +0000

    Prints a string followed by a newline

---
+++ 0x07-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * _puts_recursion   - Print a string followed by a newline
+ *
+ * @s:                 String to be printed
+ *
+ * Return:             None
+ */
+
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
---
+++ 0x07-recursion/README.md
@@ -0,0 +1,9 @@
+0-puts_recursion.c      - Print a string followed by a newline
+
+
+
+
+
+
+
+
