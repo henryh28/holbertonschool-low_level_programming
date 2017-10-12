2f49d614920e308c7fcba2351e11b4a9ae661d25
Henry Hsu <h_hsu817@yahoo.com>
2017-10-12 01:08:02 +0000

    Passes Betty, and fixed not terminating each element

--- 0x0A-malloc_free/100-strtow.c
+++ 0x0A-malloc_free/100-strtow.c
@@ -15,30 +15,26 @@ char *splitted_words(char *str)
 	int i = 0, code = 0, length = 0;
 	char *words;
 
-	/* Count length of original string */
+	if (str == NULL || str == '\0')
+		return (NULL);
+
 	for (i = 0; str[i] != '\0'; i++)
 		;
 
-	/* Dynamically assign memory based on length of original string */
 	words = malloc(sizeof(char) * (i + 1));
 
 	/* code: 0 = base, 1 = start of word */
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		/* Found a word */
 		if (code == 0 && str[i] != ' ')
 		{
 			code = 1;
 		}
-
-		/* In word processing mode, add letters to word[] */
 		if (code == 1)
 		{
 			words[length] = str[i];
 			length++;
 		}
-
-		/* Found end of word, Reset to bases status */
 		if (code == 1 && str[i] == ' ')
 		{
 			code = 0;
@@ -59,17 +55,13 @@ char *splitted_words(char *str)
 char **strtow(char *str)
 {
 	char **array;
-	int i = 0, j = 0, length = 0, number_of_words = 0, cwl = 0, lwl = 0;
+	int i = 0, j = 0, k = 0, length = 0;
+	int number_of_words = 0, cwl = 0, lwl = 0;
 	char *words;
-
-
-/*	if (str == NULL || str == "") */
-/*		return (NULL); */
 
 	words = splitted_words(str);
 	if (words == NULL)
 		return (NULL);
-
 	for (i = 0; words[i] != '\0'; i++)
 	{
 		length++;
@@ -78,24 +70,28 @@ char **strtow(char *str)
 		if (words[i] == ' ')
 		{
 			number_of_words++;
-			words[i] = '\n';
 			cwl = 0;
 		}
 	}
-
 	array = malloc(sizeof(char) * length + 1);
 	if (array == NULL)
 		return (NULL);
-
 	for (i = 0; i < number_of_words; i++)
 		array[i] = malloc(sizeof(char) * lwl);
-
 	for (i = 0; i <= number_of_words; i++)
 	{
-		for (; j < length; j++)
-			array[i][j] = words[j] != '\n' ? words[j] : '\n';
+		for (k = 0; j < length; j++, k++)
+		{
+			if (words[j] != ' ')
+				array[i][k] = words[j];
+			else
+			{
+				array[i][k] = '\0';
+				j++;
+				break;
+			}
+		}
 	}
 	array[i] = NULL;
-
 	return (array);
 }
