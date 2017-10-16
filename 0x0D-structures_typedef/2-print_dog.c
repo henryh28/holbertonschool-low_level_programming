99064c6a55da1d901ce4697031521c6c05d22146
Henry Hsu <h_hsu817@yahoo.com>
2017-10-16 19:32:35 +0000

    Reimplement return format

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -13,11 +13,13 @@
 
 void print_dog(struct dog *d)
 {
-	if (d != NULL)
+	if (d == NULL)
 	{
-		printf("Name: %s\n", d->name == NULL ? "(Nil)" : d->name);
-		printf("Age: %f\n", d->age);
-		printf("Owner: %s\n", d->owner == NULL ? "(Nil)" : d->owner);
+		return;
 	}
+
+	printf("Name: %s\n", d->name == NULL ? "(Nil)" : d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner == NULL ? "(Nil)" : d->owner);
 }
 
