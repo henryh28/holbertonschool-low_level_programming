cc9973996b3328d4ab9e5d3db6df1cc860fc21dc
Henry Hsu <h_hsu817@yahoo.com>
2017-10-16 19:17:49 +0000

    Added comment to struct

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -1,12 +1,19 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog   - Object of type dog
+ *
+ * @name:         Name of the dog
+ * @age:          Age of the dog
+ * @owner:        Owner of the dog
+ */
 
 struct dog
 {
 	char *name;
 	float age;
-        char *owner;
+	char *owner;
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
