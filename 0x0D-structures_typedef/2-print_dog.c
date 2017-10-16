dd5c8e0cc91230ca698074c626b90a5445b3dad6
Henry Hsu <h_hsu817@yahoo.com>
2017-10-16 20:12:39 +0000

    refactor

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -12,12 +12,10 @@
 
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
+	if (d != NULL)
 	{
-		return;
+		printf("Name: %s\n", d->name == NULL ? "(nil)" : d->name);
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner == NULL ? "(nil)" : d->owner);
 	}
-
-	printf("Name: %s\n", (d->name == NULL) ? "(nil)" : d->name);
-	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", (d->owner == NULL) ? "(nil)" : d->owner);
 }
