e37cf1042de1a41dfdbf83bdb4a49a58b091ddae
Henry Hsu <h_hsu817@yahoo.com>
2017-10-16 19:56:58 +0000

    Output fix

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -18,7 +18,7 @@ void print_dog(struct dog *d)
 		return;
 	}
 
-	printf("Name: %s\n", d->name == NULL ? "(Nil)" : d->name);
+	printf("Name: %s\n", (d->name == NULL) ? "(Nil)" : d->name);
 	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", d->owner == NULL ? "(Nil)" : d->owner);
+	printf("Owner: %s\n", (d->owner == NULL) ? "(Nil)" : d->owner);
 }
