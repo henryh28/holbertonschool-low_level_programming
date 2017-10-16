9301ab23946e1db3a3dfaa783a6ce3763ee776c3
Henry Hsu <h_hsu817@yahoo.com>
2017-10-16 20:00:52 +0000

    fail test

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -13,12 +13,11 @@
 
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
+	if (d != NULL)
 	{
-		return;
+		printf("Name: %s\n", d->name == NULL ? "(Nil)" : d->name);
+/*		printf("Age: %f\n", d->age); */
+		printf("Age: (Nil)\n");
+		printf("Owner: %s\n", d->owner == NULL ? "(Nil)" : d->owner);
 	}
-
-	printf("Name: %s\n", (d->name == NULL) ? "(Nil)" : d->name);
-	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", (d->owner == NULL) ? "(Nil)" : d->owner);
 }
