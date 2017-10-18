b6bcd0164875b5ec9692422436d6abe70566e4a0
Henry Hsu <h_hsu817@yahoo.com>
2017-10-18 21:18:52 +0000

    Adds error checking

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -16,6 +16,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
+	if (array == NULL || size <= 0 || cmp == NULL)
+	{
+		return (-1);
+	}
+
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]))
@@ -24,5 +29,5 @@ int int_index(int *array, int size, int (*cmp)(int))
 		}
 	}
 
-	return (0);
+	return (-1);
 }
