dbc95213e2210d20ecfdaa53807c6476161bfee6
Henry Hsu <h_hsu817@yahoo.com>
2017-10-20 08:22:42 +0000

    Output numeric arguments with separator string

---
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -0,0 +1,36 @@
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_numbers     - Print numeric arguments with @separator in between
+ *
+ * @separator:         Character used to separate the arguments when printing
+ * @n:                 Number of integers passed to the function
+ *
+ */
+
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	va_list ap;
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (i != 0 && i != n)
+		{
+			if (separator != NULL)
+			{
+				printf("%s", separator);
+			}
+		}
+
+		printf("%d", va_arg(ap, int));
+	}
+
+	va_end(ap);
+	printf("\n");
+}
