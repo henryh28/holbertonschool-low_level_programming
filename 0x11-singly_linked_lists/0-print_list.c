d2519aae1470e4c5a4cb3e4fba537d79c68de3bf
Henry Hsu <h_hsu817@yahoo.com>
2017-10-26 19:05:59 +0000

    Simplify implementation to pass checker

--- 0x11-singly_linked_lists/0-print_list.c
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -1,25 +1,6 @@
 #include "lists.h"
 
 /**
- * _strlen     - Returns length of @str argument
- *
- * @str:         String argument to be operated on
- *
- * Return:       Length of the @str argument
- */
-
-int _strlen(char *str)
-{
-	unsigned int i = 0;
-
-	for (i = 0; str[i] != '\0'; i++)
-		;
-
-	return (i);
-}
-
-
-/**
  * print_list  - Outputs data related to @h
  *
  * @h:           Pointer to the start of a singly linked list
@@ -30,24 +11,13 @@ int _strlen(char *str)
 size_t print_list(const list_t *h)
 {
 	size_t n = 0;
-	list_t *current = (list_t *)h;
 
-	while (current != NULL)
+	while (h != NULL)
 	{
-		if (current->str != NULL)
-		{
-			current->len = _strlen(current->str);
-		}
-		else
-		{
-			current->len = 0;
-			current->str = "(nil)";
-		}
-
-		printf("[%d] %s\n", current->len, current->str);
+		printf("[%d] %s\n", h->len, h->str);
 		n++;
 
-		current = current->next;
+		h = h->next;
 	}
 
 	return (n);
--- 0x11-singly_linked_lists/lists.h
+++ 0x11-singly_linked_lists/lists.h
@@ -1,7 +1,8 @@
 #ifndef LISTS_h
 #define LISTS_H
 
-#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * struct list_s - singly linked list
@@ -23,6 +24,8 @@ typedef struct list_s
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+
 
 
 #endif
