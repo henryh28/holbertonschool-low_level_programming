4e9e4b86dabe0605913a392f333f0c2a953cffa6
Henry Hsu <h_hsu817@yahoo.com>
2017-10-27 01:30:25 +0000

    Typo fix

--- 0x11-singly_linked_lists/0-print_list.c
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -15,7 +15,7 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		h->str != NULL ? printf("[%d] %s\n", h->len, h->str) :
-			printf("[0] %s\n", "(Nil)");
+			printf("[0] %s\n", "(nil)");
 
 		n++;
 		h = h->next;
