8287535350ae09a5650cadb414081e67422bf6c2
Henry Hsu <h_hsu817@yahoo.com>
2017-10-30 19:41:23 +0000

    Add a new node to the beginning of linked list argument

---
+++ 0x12-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * add_nodeint     - Add a new node at the beginning of @head
+ *
+ * @head:            Singly linked list
+ * @n:               Value of new node to be inserted to start of @head
+ *
+ * Return:           (Success) Address of the new element
+ *                   (Failure) Null
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+
+	if (head == NULL || head == NULL)
+	{
+		return (NULL);
+	}
+
+	new->n = n;
+	new->next = *head;
+	*head = new;
+
+	return (new);
+}
