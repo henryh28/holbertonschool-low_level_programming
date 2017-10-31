5727d9cfeac70b0475d3e9fe0d31162cb1e0e8d7
Henry Hsu <h_hsu817@yahoo.com>
2017-10-31 08:06:36 +0000

    Fixed typo for null check

--- 0x12-more_singly_linked_lists/2-add_nodeint.c
+++ 0x12-more_singly_linked_lists/2-add_nodeint.c
@@ -16,7 +16,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 
 	new = malloc(sizeof(listint_t));
 
-	if (head == NULL || head == NULL)
+	if (new == NULL || head == NULL)
 	{
 		return (NULL);
 	}
