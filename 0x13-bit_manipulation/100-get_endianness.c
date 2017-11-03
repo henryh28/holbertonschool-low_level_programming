e72dad68e8442106be82bd127ae86bb20fe8559c
Henry Hsu <h_hsu817@yahoo.com>
2017-11-03 03:49:56 +0000

    Checker fixes

--- 0x13-bit_manipulation/100-get_endianness.c
+++ 0x13-bit_manipulation/100-get_endianness.c
@@ -2,7 +2,7 @@
 
 /**
  * get_endianness   - Check the endianess of environment
- * 
+ *
  * Return:            (0) Big Endian environment
  *                    (1) Little Endian environment
  */
@@ -15,5 +15,5 @@ int get_endianness(void)
 	return (*c);
 }
 
-/* Reference: http://www.geeksforgeeks.org/little-and-big-endian-mystery/
- */
+/* Reference: http://www.geeksforgeeks.org/ */
+/* little-and-big-endian-mystery/ */
