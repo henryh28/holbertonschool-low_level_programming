6deeb2ef08b32e1c5884a4697c5dbdb3b329242d
Henry Hsu <h_hsu817@yahoo.com>
2018-02-19 19:41:06 +0000

    Fixed indexing error

--- 0x1B-makefiles/5-island_perimeter.py
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -7,7 +7,7 @@ def island_perimeter(grid):
     perimeter, connections = 0, 0
 
     for i in range(len(grid)):
-        for j in range(len(grid)):            
+        for j in range(len(grid[i])):
             if grid[i][j] == 1:
                 perimeter += 4
 
